358ed506fc397c71691e36059ae735bf7dad59cd
sylviamormor <sylvia.mormor@gmail.com>
2023-05-26 00:53:06 +0000

    working directory

---
+++ 0x01-intro_to_js/index.js
@@ -0,0 +1,26 @@
+
+// Set a variable myName and assign your first name as a string
+let myName = "Sylvia"; 
+
+
+console.log(typeof myName); // Output: "string"
+
+
+// Change the value of myName to your surname
+myName = "Mormor"
+
+
+
+//car object with 10 properties
+let car = {
+    make: "Hyundai",
+  model: "Elantra",
+  year: 2021,
+  color: "Red",
+  mileage: 5000,
+  V6engine: "True",
+  transmission: "Automatic",
+  fuelType: "Petrol",
+  numberOfwheels: 4,
+  isConvertible: false
+}
---
+++ 0x01-intro_to_js/numbers.js
@@ -0,0 +1,21 @@
+// Declare a variable favoriteNumber and assign your favorite number to it
+const favoriteNumber = 8;
+
+// Calculate half of your favorite number and assign it to the variable halfOfFavNum
+const halfOffavNumb = favoriteNumber / 2;
+
+// Declare a variable isaacsFavorite and assign it the value 13
+let isaacsFavorite = 13;
+
+
+// Subtract your favorite number from Isaac's favorite number
+isaacsFavorite = isaacsFavorite - favoriteNumber;
+
+
+// Change the value of isaacsFavorite to be 26 times its current value
+isaacsFavorite =  26 * isaacsFavorite;
+
+
+// Change the value of isaacsFavorite to the remainder of dividding it by 3
+isaacsFavorite = isaacsFavorite % 3;
+
